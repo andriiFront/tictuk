b444dc59598a941dbf4cb0bb600930827cbb0fc7
Andrii Badekha <badekha@gmail.com>
2021-09-16 17:20:42 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from './components/Header';
-import './App.scss';
+import { Footer } from './components/Footer';
+// import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Logo } from '../Logo/Logo';
+import './Footer.scss';
+
+export const Footer = () => (
+  <footer className="footer">
+    <Logo logoClass="footer__logo" />
+  </footer>
+);
