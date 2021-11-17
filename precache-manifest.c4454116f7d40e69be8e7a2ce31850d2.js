323d0f766cadc98c6343f1d898d38a8cb8b6db2a
Andrii <badekha@gmail.com>
2021-11-17 15:54:27 +0200

    Updates

---
+++ precache-manifest.c4454116f7d40e69be8e7a2ce31850d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e688fc18748823f468b4bd385180c81",
+    "url": "/tictuk/index.html"
+  },
+  {
+    "revision": "b2f8260bb0a094877c41",
+    "url": "/tictuk/static/css/main.1bead819.chunk.css"
+  },
+  {
+    "revision": "b7ef1165d507df2764f7",
+    "url": "/tictuk/static/js/2.6ad80fcc.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/tictuk/static/js/2.6ad80fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f8260bb0a094877c41",
+    "url": "/tictuk/static/js/main.85e7d8a9.chunk.js"
+  },
+  {
+    "revision": "32bfbff3da3b7fd2d2d5",
+    "url": "/tictuk/static/js/runtime-main.65deb403.js"
+  }
+]);
